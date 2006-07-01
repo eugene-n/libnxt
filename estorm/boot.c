b4ba88a513c5f8380ffe48ff8bc2a5b157347303
dave <dave@7835663a-1726-0410-9eea-d5d0990b5db6>
2006-07-01 11:06:21 +0000

    Start a proper Board Support Package for the NXT. Clean up estorm bootup routine.

--- estorm/boot.c
+++ estorm/boot.c
@@ -1,87 +1,75 @@
 /* EStorm bootup code */
 
-#define NXT_REG(base,addr) (*((volatile unsigned int*)(base+addr)))
+#include "nxt.h"
 
-#define PMC_BASE 0xFFFFFC00
-#define PMC_SCER NXT_REG(PMC_BASE, 0)
-#define PMC_CKGR_MOR NXT_REG(PMC_BASE, 0x20)
-#define PMC_CKGR_MCFR NXT_REG(PMC_BASE, 0x24)
-#define PMC_CKGR_PLL NXT_REG(PMC_BASE, 0x2C)
-#define PMC_MCKR NXT_REG(PMC_BASE, 0x30)
-#define PMC_SR NXT_REG(PMC_BASE, 0x68)
-
-#define MC_BASE 0xFFFFFF00
-#define MC_FMR NXT_REG(MC_BASE, 0x60)
-
-#define WDT_BASE 0xFFFFFD40
-#define WDT_MR NXT_REG(WDT_BASE, 0x4)
-
-#define RSTC_BASE 0xFFFFFD00
-#define RSTC_MR NXT_REG(RSTC_BASE, 0x8)
-
-#define EFC_INITIAL_MODE 0x340100
-#define PMC_MOS_STARTUP_TIME 64
-
-inline void estorm_oscillator_start()
+/* Configure the Flash controller with write speed settings. These
+ * settings are valid for writing everywhere but the non-volatile bits
+ * (lock, security, general-purpose NVM)
+ */
+inline void estorm_init_flash()
 {
-  int i;
-
-  PMC_CKGR_MOR = 0x2; /* Set the main oscillator to use external bypass */
-
-  /* Give it a moment to stabilize */
-  for (i = 0; i < 45; i++);
-
-  /* If we don't have a working clock signal, enable internal main oscillator */
-  if (PMC_CKGR_MCFR & (0x10000) == 0)
-    {
-      PMC_CKGR_MOR = (PMC_MOS_STARTUP_TIME << 8) & 0x1;
-
-      /* Wait for oscillator to become available */
-      while (PMC_SR & 0x1 != 1);
-    }
-
-  /* Switch the main clock over to use the MOS */
-  PMC_MCKR = 1; /* Main clock, no prescaler */
-
-  /* Wait for master clock ready */
-  while (PMC_SR & 0x8 != 1);
+  /* The values are those provided by the LEGO OS. */
+  NXT_MC->MC_FMR = MC_FMR_FMCN_FLASH | MC_FMR_FWS_V(1);
 }
 
-void estorm_usb_clock_start()
+/* Initialize the watchdog timer, by disabling it. We do not need it
+ * in our application.
+ */
+inline void estorm_init_watchdog()
 {
-  /* First, configure the PLL oscillator with the following values:
-   *  - DIV = 14
-   *  - PLLCOUNT = 63
-   *  - OUT = 0
-   *  - MUL = 48
-   *  - USBDIV = 2
+  NXT_WDT->WDT_MR = WDT_MR_WDDIS;
+
+}
+
+/* Boot the quartz oscillator, step the CPU up to 48MHz, and enable
+ * USB clocks.
+ */
+inline void estorm_init_clocks()
+{
+  /* Enable internal main oscillator */
+  NXT_PMC->CKGR_MOR = (CKGR_MOS_START_TIME | CKGR_MOR_ENABLE);
+
+  /* Wait for oscillator to stabilize */
+  while (NXT_PMC->PMC_SR & PMC_SR_MOSCS == 0);
+
+  /* Initialize the PLL clock. The quartz runs at 18.432MHz, we run
+   * the PLL at ~96MHz.
    */
-  PMC_CKGR_PLL = (2 << 28) & (48 << 16) & (63 << 8) & 14;
+  NXT_PMC->CKGR_PLLR = (CKGR_PLL_DIV_V(0xE) | CKGR_PLL_COUNT_V(0x1C) |
+                        CKGR_PLL_MUL_V(0x48) | CKGR_PLL_USBDIV_2);
 
   /* Wait for the PLL to lock */
-  while (PMC_SR & 0x4 != 1);
+  while (NXT_PMC->PMC_SR & PMC_SR_PLL_LOCKED == 0);
 
-  /* Enable USB device port clock (PLL interface) */
-  PMC_SCER = 0x80;
-  /* Enable USB peripherial clock (non-PLL) */
-  
+  /* Wait for master clock ready (just in case) */
+  while (NXT_PMC->PMC_SR & PMC_SR_MCKRDY == 0);
+
+  /* Set the master clock prescaler to /2 */
+  NXT_PMC->PMC_MCKR = PMC_MCKR_PRES_2 | PMC_MCKR_CSS_SLOW;
+
+  /* Wait for master clock ready */
+  while (NXT_PMC->PMC_SR & PMC_SR_MCKRDY == 0);
+
+  /* Switch the main clock over to the PLL */
+  NXT_PMC->PMC_MCKR = PMC_MCKR_PRES_2 | PMC_MCKR_CSS_PLL;
+
+  /* Wait for master clock ready */
+  while (NXT_PMC->PMC_SR & PMC_SR_MCKRDY == 0);
+}
+
+/* Initialize the reset controller
+ *
+ */
+inline void estorm_init_reset_control()
+{
+  NXT_RSTC->RSTC_MR = (RSTC_MR_ERSTL_V(4) | RSTC_MR_URSTEN
+                       | RSTC_MR_KEY_VAL);
 }
 
 void estorm_boot()
 {
-  /* Configure the EFC controller. We never need to touch this
-     again. */
-  MC_FMR = EFC_INITIAL_MODE;
-
-  /* Start the main oscillator and switch to using it */
-  estorm_oscillator_start();
-
-  /* Disable the watchdog timer */
-  WDT_MR = (1 << 15);
-
-  /* Enable user resets */
-  RSTC_MR = (4 << 8) & 0x1;
-
-  /* Configure and enable the USB clocks */
-  estorm_usb_clock_start();
+  estorm_init_flash();    /* Flash controller init */
+  estorm_init_watchdog(); /* Silence the watchdog  */
+  estorm_init_clocks();   /* Start clocks          */
+  estorm_init_reset_control(); /* Allow hardware resets */
 }
